6567f6e9198816df20881fe6eaf230b7513339ed
gaa7par <gaa7par@gmail.com>
2017-03-14 14:52:19 +0100

    Third iteration

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,4 +1,13 @@
 numbers = []
+sum = 0
 
-7.times { numbers << rand(1..20) }
-numbers.sort.slice(1..5).each { |number| puts number }
+while sum < 55 do
+  numbers = []
+
+  7.times { numbers << rand(1..20) }
+
+  numbers = numbers.sort.slice(1..5)
+  sum = numbers.sum
+end
+
+numbers.each { |number| puts number }
