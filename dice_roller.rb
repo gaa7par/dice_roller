a3db32db7715148b8d2ff597c48ff8f313296fa6
gaa7par <gaa7par@gmail.com>
2017-03-14 18:55:44 +0100

    Fifth iteration

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,12 +1,18 @@
 numbers = []
 sum = 0
+change_slice = false
+
+if rand(1..30) <= 7
+  change_slice = true if rand(1..30) == 1
+end
 
 while sum < 55 do
   numbers = []
 
   7.times { numbers << rand(1..20) }
 
-  numbers = numbers.sort.slice(1..5)
+  numbers = change_slice ? numbers.sort.slice(2..6) : numbers.sort.slice(1..5)
+
   numbers << rand(1..20)
   sum = numbers.sum
 end
