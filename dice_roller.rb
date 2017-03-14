48a54658c7d02f49ac6150940eda57fddf6393d3
gaa7par <gaa7par@gmail.com>
2017-03-14 20:26:55 +0100

    Sixth iteration

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,9 +1,13 @@
 numbers = []
 sum = 0
 change_slice = false
+change_add = false
 
-if rand(1..30) <= 7
+random_number = rand(1..30)
+if random_number <= 7
   change_slice = true if rand(1..30) == 1
+elsif (24..30).include?(random_number)
+  change_add = true
 end
 
 while sum < 55 do
@@ -13,7 +17,12 @@ while sum < 55 do
 
   numbers = change_slice ? numbers.sort.slice(2..6) : numbers.sort.slice(1..5)
 
-  numbers << rand(1..20)
+  if change_add
+    numbers << 25
+  else
+    numbers << rand(1..20)
+  end
+
   sum = numbers.sum
 end
 
